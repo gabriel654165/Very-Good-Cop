04683302fa8411dd190fd71df644bad86c5847e5
Gabriel <gabriel.medoukali@epitech.eu>
2023-03-25 00:27:14 +0900

    feat: distance of grenades and bouncing on the walls and characters

--- scripts/weapons/Grenade.gd
+++ scripts/weapons/Grenade.gd
@@ -4,7 +4,7 @@ class_name Grenade
 #not used
 @export var impact_area : Vector2 = Vector2(2, 2)
 @export var max_launch_distance : float = 400
-@export var landing_precision : int = 50
+@export var landing_precision : int = 1
 
 @onready var explosion_timer = $ExplosionTimer
 @onready var explosion_area = $ExplosionArea
@@ -19,34 +19,42 @@ func _ready():
 	randomize()
 	scale = scale * size
 
+var remainder : float = 0.0
+
 func _physics_process(delta):
 	if direction != Vector2.ZERO:
-		
 		#lerp de velocity : + for au debut et reduit avec le temps
 		var velocity = direction * GlobalFunctions.get_speed(speed, delta)
 		global_position += velocity
-		
+	
 	if speed == 0 or direction == Vector2.ZERO:
 		return
 	
 	var range_safe := 5
 	var distance_traveled := lauching_position.distance_to(global_position)
 	if distance - range_safe <= distance_traveled and distance_traveled <= distance + range_safe:
-		stop_or_bounce()
+		stop()
 
-func stop_or_bounce():
-	#si la distance parcourue n'était pas finit :
-	# distance = distance / 2
-	# direction = -direction
+func stop():
 	speed = 0
 	direction = Vector2.ZERO
 	explosion_timer.start()
 
+func bounce(body):
+	
+	#depasse le canvas en x
+	if global_position.x < body.global_position.x or global_position.x > body.global_position.x:
+		direction.x = -direction.x
+		global_position.x = global_position.x + direction.x
+	
+	#dépasse le canvas en y
+	if global_position.y < body.global_position.y or global_position.y > body.global_position.y:
+		direction.y = -direction.y
+		global_position.y = global_position.y + direction.y
+
 func _on_body_entered(body):
-	if body.get_name() == "Walls":
-		stop_or_bounce()
-	if body is Character:
-		stop_or_bounce()
+	if body.get_name() == "Walls" or body is Character:
+		bounce(body)
 
 func set_lauching_position(position: Vector2):
 	self.lauching_position = position
@@ -82,4 +90,11 @@ func _on_explosion_area_body_entered(body):
 
 func _on_explosion_area_body_exited(body):
 	#bodies.remove(body)
+	pass
+
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+#	var area_shape = self
+#	var shape = body.get_shape(area_shape)
+#	var contacts = shape.collide_and_get_contacts(shape_owner_get_transform(shape_find_owner(local_shape_index)))
+#	print("contacts : ", contacts)
 	pass
