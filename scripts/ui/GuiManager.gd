9168cb86f63be34e0015c3c2b33e0ec1176a5127
Gabriel <gabriel.medoukali@epitech.eu>
2023-04-16 04:27:59 +0900

    feat: timer gui manager added in guiManager

--- scripts/ui/GuiManager.gd
+++ scripts/ui/GuiManager.gd
@@ -7,4 +7,5 @@ class_name GuiManager
 @onready var pop_up_points_manager = $PopUpPointsManager
 @onready var panel_points_manager = $PanelPointsManager
 @onready var panel_kills_manager = $PanelKillsManager
+@onready var panel_timer_manager = $PanelTimerManager
 @onready var weapon_panel_manager = $WeaponPanelManager
