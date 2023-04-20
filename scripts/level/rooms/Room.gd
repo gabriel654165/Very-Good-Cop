54e02f9b3be8c5e10c06514de5f57a2a178580ee
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2023-04-20 16:11:19 +0900

    feat: generic spawn with roomconfig

--- scripts/level/rooms/Room.gd
+++ scripts/level/rooms/Room.gd
@@ -7,28 +7,47 @@ class_name Room
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	assert(room_config != null, "Add a room config to your room")
-	spawn_enemies()
-	#spawn_powerups()
+	spawn_with_room_config("Patrols", get_patrol_paths, spawn_enemy)
+	spawn_with_room_config("PowerUpPoints", get_children_positions_array, spawn_powerups)
 
-# 
+
+func spawn_with_room_config(
+	node_name:String, 
+	process_node:Callable, # Takes the node as a parameter and returns an array. The elements of the array will be passed as a parameter to spawn_function
+	spawn_function:Callable,
+	random_nb_to_spawn := true, # Will call spawn_function between 1 time and process_node's array size time if true, else will call the func for each element of the array  
+	shuffle := true # shuffle process_node's array before spawning
+	):
+	var node:Node = room_config.find_child(node_name)
+	var processed_node_as_array:Array = process_node.call(node)
+
+	var nb_to_spawn:int = randi_range(1, processed_node_as_array.size()) if random_nb_to_spawn else processed_node_as_array.size()
+
+	if shuffle:
+		processed_node_as_array.shuffle()
+
+	for element in processed_node_as_array.slice(0, nb_to_spawn):
+		spawn_function.call(element)
+
+
+func get_children_positions_array(node:Node):
+	return node.get_children().map(func(node:Node2D): return node.global_position)
+
+#
 # Spawn powerups
 #
 
-func spawn_powerups():
-	var packed_enemy:PackedScene = preload("res://scenes/characters/enemies/pistolero.tscn")
 
-	var powerups_points_node:Node = room_config.find_child("PowerUpPoints")
-	var powerups_spawn_points:Array = powerups_points_node.get_children().map(func(node:Node2D): return node.global_position)
+func spawn_powerups(position:Vector2):
+	var power_ups:Array[PackedScene] = [
+#		preload("res://scenes/powerups/SlowMotion.tscn"),
+#		preload("res://scenes/powerups/Speed.tscn"),
+#		preload("res://scenes/powerups/Damage.tscn"),
+	]
+	var power_up = power_ups.pick_random().instantiate()
+	add_child(power_up)
+	power_up.global_position = position
 
-	var nb_powerup_to_spawn:int = randi_range(0, powerups_spawn_points.size())
-
-	powerups_spawn_points.shuffle()
-
-#	for patrol_points in patrol_paths_vectors.slice(0, nb_enemy_to_spawn):
-#		var enemy:Enemy = packed_enemy.instantiate()
-#		enemy.patrol_points = patrol_points
-#		add_child(enemy)
-#		enemy.global_position = patrol_points[0]
 
 #
 # Spawn enemies
@@ -48,27 +67,18 @@ func get_vectors_from_patrol_path(patrol_path_node:Node) -> Array:
 	)
 
 
-func get_patrols_paths() -> Array:
-	var patrols_node:Node = room_config.find_child("Patrols")
-	var nb_of_patrols:int = patrols_node.get_child_count() if patrols_node != null else 0
+func get_patrol_paths(node:Node) -> Array:
+	var nb_of_children:int = node.get_child_count() if node != null else 0
 
-	if nb_of_patrols == 0:
+	if nb_of_children == 0:
 		return []
-	return patrols_node.get_children().map(get_vectors_from_patrol_path)
+	return node.get_children().map(get_vectors_from_patrol_path)
 
 
-func spawn_enemies():
+func spawn_enemy(patrol_points:Array):
 	var packed_enemy:PackedScene = preload("res://scenes/characters/enemies/pistolero.tscn")
 
-	var patrol_paths_vectors:Array = get_patrols_paths()
-
-	var nb_enemy_to_spawn:int = randi_range(1, patrol_paths_vectors.size())
-
-	patrol_paths_vectors.shuffle()
-
-	for patrol_points in patrol_paths_vectors.slice(0, nb_enemy_to_spawn):
-		var enemy:Enemy = packed_enemy.instantiate()
-		enemy.patrol_points = patrol_points
-		add_child(enemy)
-		enemy.global_position = patrol_points[0]
-
+	var enemy:Enemy = packed_enemy.instantiate()
+	enemy.patrol_points = patrol_points
+	add_child(enemy)
+	enemy.global_position = patrol_points[0]
