2eaac39044dd2a3828a54e0ad39b922364659e5a
samuel <besseausamuel@gmail.com>
2023-04-21 20:10:38 +0200

    fix: DetectableTargetManager autoload

--- scripts/AI/DetectableTarget.gd
+++ scripts/AI/DetectableTarget.gd
@@ -2,8 +2,8 @@ extends Node2D
 class_name DetectableTarget
 
 func _enter_tree():
-	DetectableTargetManager.register(self);
+	DetectableTargetManager.register(self)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _exit_tree():
-	DetectableTargetManager.deregister(self);
+	DetectableTargetManager.deregister(self)
