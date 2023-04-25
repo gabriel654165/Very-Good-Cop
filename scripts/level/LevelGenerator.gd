ccf76a2ee6f7d045c4599423fa6f42059de1cfe8
Edouard SENGEISSEN <58398928+edouard-sn@users.noreply.github.com>
2023-04-25 15:59:56 +0900

    cleanup: random commented code

--- scripts/level/LevelGenerator.gd
+++ scripts/level/LevelGenerator.gd
@@ -42,7 +42,6 @@ func generate():
 	# NOTE: Find a better rng
 	var number_of_rooms:int = BaseNbOfRoomss + (GlobalVariables.level + randi_range(0, 3))
 
-	#print("Generating level with " + str(number_of_rooms) + " rooms")
 
 	reset_dungeon_layout()
 	generate_dungeon_layout(number_of_rooms)
