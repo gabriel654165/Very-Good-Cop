88617480652f8fdb8b0aaa978ebdd23e5d61299e
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2023-05-03 16:59:37 +0900

    clean: scene hierarchy and useless code

--- scenes/objects/ExplosiveBarrel.gd
+++ scenes/objects/ExplosiveBarrel.gd
@@ -1,8 +1,8 @@
 extends StaticBody2D
 
 @onready var health := $Health as Health
-@onready var animation := $Animation as AnimationPlayer
-@onready var particle_player := $ParticlePlayer as AnimationPlayer
+@onready var animation := $Explosion/Animation as AnimationPlayer
+@onready var particle_player := $ExplosionParticle/Animation as AnimationPlayer
 @onready var explosion_area := $ExplosionArea as Area2D
 
 @export var damage := 35.0
@@ -23,11 +23,6 @@ func explode():
 
 	particle_player.play("explosion_praticle")
 
-#	var explosion_particles_scene := preload("res://scenes/particles/Explosion.tscn")
-#	var explosion_particle := explosion_particles_scene.instantiate()
-#	add_child(explosion_particle)
-#	explosion_particle.global_position = global_position
-
 	var bodies := explosion_area.get_overlapping_bodies()
 
 	for body in bodies:
@@ -37,9 +32,6 @@ func explode():
 			body.handle_hit(self, damage)
 		if body.has_method("apply_force"):
 			var normalized_impact_direction = (body.global_position - self.global_position).normalized()
-			print(body.name)
-			print(normalized_impact_direction)
-			print(impact_force)
 			body.apply_force(body, normalized_impact_direction, impact_force)
 		if body is Grenade:
 			assert(body.has_method("explode"))
