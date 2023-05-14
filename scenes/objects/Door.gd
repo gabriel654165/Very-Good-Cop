3c084c752cb87b5de6fb0ce6adad041df19434ab
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2023-05-14 17:17:46 +0900

    feat: better values and explanations

--- scenes/objects/Door.gd
+++ scenes/objects/Door.gd
@@ -1,10 +1,21 @@
 extends Area2D
 
 @onready var starting_rotation := self.rotation
+
 @export var decreasing_speed := 0.25
+
+# Tweak for the base speed of the door opening
 @export var opening_deg_angle := 25
+
+# Two maximum degrees: starting_rotation(rad) + max_deg_angle and starting_rotation(rad) - max_deg_angle
 @export var max_deg_angle := 150
-@export var distance_from_the_hinge_force_factor := 800.0
+
+# Tweak for influence of the distrance between body and hinge on the speed
+@export var distance_from_the_hinge_force_factor := 700.0
+
+# Tweak those for maximum door speed 
+@export var max_door_speed_rad := 15
+@export var min_magnitude := 25
 
 var door_speed_rad := 0.0
 
@@ -22,13 +33,15 @@ func _on_body_entered(body:Node2D):
 	var rotation_is_clockwise := body_collision_angle > 0
 
 	var magnitude := door_origin_to_body.length()
+	magnitude = min_magnitude if magnitude < min_magnitude else magnitude
 
 	var magnitude_force_factor : float = distance_from_the_hinge_force_factor/magnitude # The closer we are to door's hinge, the faster it will go
 
 	var clockwise_factor := 1 if rotation_is_clockwise else -1 
 	
 	door_speed_rad = deg_to_rad(opening_deg_angle) * (magnitude_force_factor) * clockwise_factor
-
+	door_speed_rad = clamp(door_speed_rad, -max_door_speed_rad, max_door_speed_rad)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
