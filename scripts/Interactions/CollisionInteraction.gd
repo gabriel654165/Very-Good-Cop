78a2f524ab5db27e091419c30543db7b454a7581
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2023-05-18 11:27:25 +0900

    feat: shared interaction data and power up text

--- scripts/Interactions/CollisionInteraction.gd
+++ scripts/Interactions/CollisionInteraction.gd
@@ -6,6 +6,11 @@ class_name CollisionInteraction
 
 var body_touching : Node = null
 
+func update_data(common_data:Dictionary):
+	if !common_data.has('CollisionInteraction'):
+		common_data['CollisionInteraction'] = Dictionary()
+	common_data['CollisionInteraction']['body_touching'] = body_touching
+
 func get_body_touching() -> Character:
 	var bodies = area2D.get_overlapping_bodies()
 	var body_first_priority : Character = null
@@ -17,10 +22,10 @@ func get_body_touching() -> Character:
 			body_first_priority = body
 	
 	if body_first_priority != null:
-		#print("interaction : collision = TRUE by : ", body_first_priority.name)
+		print("interaction : collision = TRUE by : ", body_first_priority.name)
 		body_touching = body_first_priority
 		is_triggered = true
-	
+
 	return body_first_priority
 
 func _on_body_entered(body):
