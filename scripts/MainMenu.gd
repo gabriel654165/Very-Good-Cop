60edb90bf08da172608e938066216dbda90a9021
Gabriel <gabriel.medoukali@epitech.eu>
2023-06-14 19:33:37 +0900

    feat: main menu screen

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -4,20 +4,31 @@ class_name MainMenu
 
 @onready var cursor_manager = $CanvasLayer/CursorManager
 
+@export var animator : AnimationPlayer
 
 func _ready():
 	cursor_manager.set_active(true)
 	cursor_manager.cursor.active_mode_ui()
+	animator.play("unzoom_and_fade_out")
+
+
+func start_new_game():
+	get_tree().change_scene_to_file("res://scenes/main/MainScene.tscn")
+
+
+func start_training_game():
+	get_tree().change_scene_to_file("res://scenes/tuto/TrainingScene.tscn")
 
 
 func _on_play_button_up():
 	GlobalFunctions.reset_player_levels()
-	get_tree().change_scene_to_file("res://scenes/main/MainScene.tscn")
+	animator.play("zoom_and_fade")
 
 
 func _on_training_button_button_up():
-	get_tree().change_scene_to_file("res://scenes/tuto/TrainingScene.tscn")
+	animator.play("zoom_and_fade_2")
 
 
 func _on_quit_button_up():
 	get_tree().quit()
+
